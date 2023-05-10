7a1247b1b973633c35d1f7513bcd853ac245faa4
Samira Boudjemai <sboudjemai@xpanxion.com>
2023-05-10 10:33:53 -0400

    upadate ex3

--- modern-javascript-for-react-devs/students/15/main.js
+++ modern-javascript-for-react-devs/students/15/main.js
@@ -12,26 +12,65 @@
 
 //const ex2 = () => {
     // TODO...
-}
 
-const ex3 = () => {
-    // TODO...
-}
-
-//
-// Your functions here...
-//
-
-const countNumbers = (array) => {
-    let numberCounter = 0;
-for (let i = 0; i < array.length ; i++) { 
-    if(typeof array[i] == 'number') numberCounter ++;
+    const arr = [12, 55, 2, 22, 11];
+    let min = arr[0]; 
+   
+    // console.log(min);
+  //}
+ 
+  const minNumber = (arr) => {
+     for (let i = 0; i < arr.length ; i++) { 
+     if (arr[i] < min){
+         min = arr[i];
+     
+     }
+        }
+        return min
     }
-    return numberCounter;
-}
+    console.log(minNumber(arr));
 
-const main = async () => {
-    ex1();
-}
 
-main();
+ // const ex3 = () => {
+ //     // TODO...
+ // }
+ 
+ //
+ // Your functions here...
+ //
+ 
+ // const countNumbers = (array) => {
+ //     let numberCounter = 0;
+ // for (let i = 0; i < array.length ; i++) { 
+ //     if(typeof array[i] == 'number') numberCounter ++;
+ //     }
+ //     return numberCounter;
+ // }
+
+ const array1 = [1, 2]
+const array2 = ['a', 'b', 'c', 'd', 'e'];
+
+const interleave = (array1, array2) => {
+    if (array1.length !== array2.length) {
+      return 'ERROR: Array length mismatch';
+    }
+  
+    let result = '';
+    for (let i = 0; i < array1.length; i++) {
+      result += array1[i] + array2[i];
+    }
+  
+    return result;
+  }
+  console.log(interleave(array1, array2));
+ 
+
+ 
+ // const main = async () => {
+ //     ex1();
+ //}
+ 
+ 
+ 
+ // main();
+ 
